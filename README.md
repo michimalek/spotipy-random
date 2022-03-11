5887ba4da52a71c417692556346d9241fe58bfb9
Lennard Beers <l.beers@outlook.de>
2022-03-11 16:53:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Spotipy-random
+
+## Description
+
+This package extends the already existing [spotipy API client library](https://spotipy.readthedocs.io/en/2.19.0/) with a randomized search functionallity. Both libraries are based on the official [Spotify API](https://developer.spotify.com/documentation/).
+
+# Installation
+
+This package can be installed through pip with the following command:
+
+```
+pip install spotipy-random
+```
+
+The spotipy package is also required and can be installed with:
+
+```
+pip install spotipy
+```
+
+# Usage
+
+To use this library first create a new spotipy client object including your personal Spotify API client_id and client_secret. Then the `get_random` method can be used to search for random tracks, albums and more. You can also specify search filters, although some filter will not work with each search type (Check details in the [Spotify API documentation](https://developer.spotify.com/documentation/web-api/reference/#/operations/search)).
+
+```py
+from spotipy import Spotfiy
+from spotipy-random import get_random
+
+spotify_client = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_APP_CLIENT_ID",client_secret="YOUR_APP_CLIENT_SECRET"))
+
+random_pop_song_json: str = get_random(spotify: spotify_client, type="track", genre="pop")
+```
+
+# How it works
+
+The randomization works by selecting a random letter to search for and randomly picking one of the result elements. The number of results can be changed with the `limit` parameter. The `offset_min` and `offset_max` parameters can optionally be used to influence the search result [offset](https://developer.spotify.com/documentation/web-api/reference/#/operations/search). 
---
+++ src/spotipy_random/spotipy_random.py
@@ -0,0 +1,88 @@
+from spotipy import Spotify
+from typing import Any, Optional
+import random
+
+
+def get_wildcard() -> str:
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    wildcard = random.choice(alphabet)
+
+    rand = random.randint(0,2)
+    if rand == 0:
+        wildcard = f"%{wildcard}"
+    if rand == 1:
+        wildcard = f"{wildcard}%"
+    if rand == 2:
+        wildcard = f"%{wildcard}%"
+
+    return wildcard
+
+
+def get_random(spotify: Spotify,
+               limit: int = 10,
+               offset_min: int = 0,
+               offset_max: int = 20,
+               type: str = "track",
+               market: Any = None,
+               album: str = None,
+               artist: str = None,
+               track: str = None,
+               year: str = None,
+               upc: str = None,
+               tag_hipster: bool = None,
+               tag_new: bool = None,
+               isrc: str = None,
+               genre: str = None,
+               ) -> Optional[Any]:
+
+    if offset_max > 1000:
+        raise ValueError("The maximum allowed offset is 1000.")
+
+    types: list = type.split(',')
+    offset: int = random.randint(offset_min, offset_max)
+
+    wildcard: str = get_wildcard()
+    q: str = f"{wildcard}"
+
+    if len(types) == 0:
+        raise ValueError("Type must contain at least one valid search type.")
+
+    artist_filter_supported: bool = not (("playlist" or "show" or "episode") in types)
+    album_filter_supported: bool = not (("artist" or "playlist" or "show" or "episode") in types)
+    genre_filter_supported: bool = not (("album" or "playlist" or "show" or "episode") in types)
+    track_filter_supported: bool = not (("artist" or "album" or "playlist" or "show" or "episode") in types)
+    year_filter_supported: bool = not (("artist" or "playlist" or "show" or "episode") in types)
+    isrc_filter_supported: bool = not (("artist" or "album" or "playlist" or "show" or "episode") in types)
+    upc_filter_supported: bool = not (("artist" or "track" or "playlist" or "show" or "episode") in types)
+    tag_hipster_filter_supported: bool = not (("artist" or "track" or "playlist" or "show" or "episode") in types)
+    tag_new_filter_supported: bool = not (("artist" or "track" or "playlist" or "show" or "episode") in types)
+
+    if artist_filter_supported and artist is not None:
+        q += f" artist:{artist}"
+
+    if album_filter_supported and album is not None:
+        q += f" album:{album}"
+
+    if track_filter_supported and track is not None:
+        q += f" track:{track}"
+
+    if year_filter_supported and year is not None:
+        q += f" year:{year}"
+
+    if genre_filter_supported and genre is not None:
+        q += f" genre:{genre}"
+
+    if isrc_filter_supported and isrc is not None:
+        q += f" isrc:{isrc}"
+
+    if upc_filter_supported and upc is not None:
+        q += f" upc:{upc}"
+
+    if tag_hipster_filter_supported and tag_hipster is not None:
+        q += f" {'tag:hipster' if tag_hipster else ''}"
+
+    if tag_new_filter_supported and tag_new is not None:
+        q += f" {'tag:new' if tag_new else ''}"
+
+    result = spotify.search(q, limit, offset, type, market)
+    return result
